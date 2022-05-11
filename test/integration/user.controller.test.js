7f2920211f66f7411fa03a2e27f0c79a3d655c0a
TurboSjaantje <annedvdmeulen@gmail.com>
2022-05-11 12:01:58 +0200

    added share-a-meal-create.sql

--- test/integration/user.controller.test.js
+++ test/integration/user.controller.test.js
@@ -1,37 +1,38 @@
-process.env.DB_DATABASE = process.env.DB_DATABASE || 'share-a-meal-testdb'
+process.env.DB_DATABASE = process.env.DB_DATABASE || "share-a-meal-testdb";
 
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-let server = require("../../index");
-let database = [];
+const server = require("../../index");
+const assert = require("assert");
+require("dotenv").config();
+const dbconnection = require("../../database/dbconnection");
 
 chai.should();
 chai.use(chaiHttp);
-
+	
 describe("Manage users", () => {
-	describe("UC-201 add user /api/user", () => {
-		beforeEach((done) => {
-			database = [];
-			done();
-		});
+    describe("UC-201 add user /api/user", () => {
+        beforeEach((done) => {
+            database = [];
+            done();
+        });
 
-		it("When a required input is missing, a valid error should be returned", (done) => {
-			chai
-				.request(server)
-				.post("/api/user")
-				.send({
-					//name: "Daan van der Meulen",
-					emailAdress: "daanvdm@hotmail.com",
-				})
-				.end((err, res) => {
-					res.should.be.an("object");
-					let { status, result } = res.body;
-					status.should.equals(400);
-					result.should.be
-						.a("string")
-						.that.equals("Name must be a string!");
-					done();
-				})
-		})
-	})
-})
+        it("When a required input is missing, a valid error should be returned", (done) => {
+            chai.request(server)
+                .post("/api/user")
+                .send({
+                    //name: "Daan van der Meulen",
+                    emailAdress: "daanvdm@hotmail.com",
+                })
+                .end((err, res) => {
+                    res.should.be.an("object");
+                    let { status, result } = res.body;
+                    status.should.equals(400);
+                    result.should.be
+                        .a("string")
+                        .that.equals("Name must be a string!");
+                    done();
+                });
+        });
+    });
+});
