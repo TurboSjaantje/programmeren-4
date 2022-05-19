7a01260827dbc57de8eefe2b0f60b1860693080a
TurboSjaantje <73107330+TurboSjaantje@users.noreply.github.com>
2022-05-19 20:28:09 +0200

    test

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,22 +1,31 @@
 const mysql = require('mysql2');
+const logger = require('../src/config/config').logger;
 require('dotenv').config();
 
-var pool = mysql.createPool({
+const databaseConfiguration = {
 	connectionLimit: 10,
 	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
-	multipleStatements: true
+	multipleStatements: true,
+};
+
+logger.debug(databaseConfiguration);
+
+var pool = mysql.createPool(databaseConfiguration);
+
+pool.on('connection', function (connection) {
+	logger.debug(`Connected to database '${connection.config.database}'`);
 });
 
-module.exports = pool;
-
 pool.on('acquire', function (connection) {
-	console.log('Connection %d acquired', connection.threadId);
+	logger.debug('Connection %d acquired', connection.threadId);
 });
 
 pool.on('release', function (connection) {
-	console.log('Connection %d released', connection.threadId);
+	logger.debug('Connection %d released', connection.threadId);
 });
+
+module.exports = pool;
