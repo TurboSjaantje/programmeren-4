4566e1a4bc2aa06ad049953326fa7984f236a469
TurboSjaantje <73107330+TurboSjaantje@users.noreply.github.com>
2022-05-19 20:14:37 +0200

    Added some authentication shit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const authRoutes = require('./src/routes/authentication.routes');
+const logger = require('./src/config/config').logger;
+
 require('dotenv').config();
 const port = process.env.PORT;
 
@@ -11,31 +14,42 @@ const userRouter = require('./src/routes/user.routes');
 //Log all requests
 app.all('*', (req, res, next) => {
 	const method = req.method;
-	console.log(`method ${method} aangeroepen`);
+	logger.debug(`Method ${method} is aangeroepen`);
 	next();
 });
 
 //All valid routes
 app.use(userRouter);
+app.use(authRoutes);
 
 //All unvalid routes
 app.all('*', (req, res) => {
 	res.status(404).json({
 		status: 404,
-		result: 'end-point not found',
+		result: 'End-point not found',
 	});
 });
 
 //Error Handling
 app.use((err, req, res, next) => {
+	logger.debug('Error handler called.');s
 	res.status(err.status).json(err);
 });
 
 //Start server
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	logger.debug(`Example app listening on port ${port}`)
 });
 
-//test
+//SIGN IN
+process.on('SIGINT', () => {
+    logger.debug('SIGINT signal received: closing HTTP server')
+    dbconnection.end((err) => {
+        logger.debug('Database connection closed')
+    })
+    app.close(() => {
+        logger.debug('HTTP server closed')
+    })
+})
 
 module.exports = app;
