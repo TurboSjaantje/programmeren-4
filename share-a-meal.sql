a16e10b3dc2f176748d804c711e2de4bf310e871
TurboSjaantje <73107330+TurboSjaantje@users.noreply.github.com>
2022-05-20 22:38:39 +0200

    installed new modules

--- share-a-meal.sql
+++ share-a-meal.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `meal`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `meal` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,a
   `isActive` tinyint NOT NULL DEFAULT '0',
   `isVega` tinyint NOT NULL DEFAULT '0',
   `isVegan` tinyint NOT NULL DEFAULT '0',
