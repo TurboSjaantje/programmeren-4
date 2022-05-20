1ff9b36a5672db5d6885c66560b4ae3303e96940
TurboSjaantje <73107330+TurboSjaantje@users.noreply.github.com>
2022-05-20 11:15:38 +0200

    added remaining meal functionality

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -2,10 +2,133 @@ const dbconnection = require('../../database/dbconnection');
 const logger = require('../config/config').logger;
 const assert = require('assert');
 const { resourceUsage } = require('process');
+const { rollback } = require('../../database/dbconnection');
 
 let controller = {
+	validateMeal: (req, res, next) => {
+		let meal = req.body;
+		let { name, description, imageUrl, maxAmountOfParticipants, price } =
+			meal;
+
+		// validates meal attributes
+		try {
+			assert(typeof name === 'string', 'Name should be a string');
+			assert(
+				typeof description === 'string',
+				'Description should be a string'
+			);
+			assert(
+				typeof imageUrl === 'string',
+				'Image URL should be a string'
+			);
+			assert(
+				typeof maxAmountOfParticipants === 'number',
+				'maxAmountofParticipants should be a number'
+			);
+			assert(typeof price === 'number', 'Price should be a number');
+
+			next();
+		} catch (err) {
+			const error = {
+				status: 400,
+				message: err.message,
+			};
+
+			next(error);
+		}
+	},
+	validateMealUpdate: (req, res, next) => {
+		let meal = req.body;
+		let { name, description, imageUrl, maxAmountOfParticipants, price } =
+			meal;
+
+		// validates meal attributes
+		try {
+			assert(typeof name === 'string', 'Name should be a string');
+			assert(
+				typeof description === 'string',
+				'Description should be a string'
+			);
+			assert(
+				typeof imageUrl === 'string',
+				'Image URL should be a string'
+			);
+			assert(
+				typeof maxAmountOfParticipants === 'number',
+				'maxAmountofParticipants should be a number'
+			);
+			assert(typeof price === 'number', 'Price should be a number');
+
+			next();
+		} catch (err) {
+			const error = {
+				status: 400,
+				message: err.message,
+			};
+
+			next(error);
+		}
+	},
 	// UC-301
-	registerMeal: (req, res, next) => {},
+	registerMeal: (req, res, next) => {
+		let meal = req.body;
+		const cookId = req.userId;
+		logger.debug(meal);
+		dbconnection.getConnection(function (err, connection) {
+			if (err) throw err; // not connected!
+
+			// adds new meal
+			connection.query(
+				'INSERT INTO meal (datetime, maxAmountOfParticipants, price, imageUrl, cookId, name, description, isActive, isVega, isVegan, isToTakeHome) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);',
+				[
+					meal.dateTime,
+					meal.maxAmountOfParticipants,
+					meal.price,
+					meal.imageUrl,
+					cookId,
+					meal.name,
+					meal.description,
+					meal.isActive,
+					meal.isVega,
+					meal.isVegan,
+					meal.isToTakeHome,
+				],
+				function (error, results, fields) {
+					if (error) {
+						logger.debug(error);
+						connection.release();
+						const newError = {
+							status: 409,
+							message: `Meal not created.`,
+						};
+						next(newError);
+					} else {
+						connection.query(
+							'SELECT * FROM meal ORDER BY id DESC LIMIT 1;',
+							function (error, results, fields) {
+								if (error) throw error;
+								connection.query(
+									'SELECT * FROM user WHERE id = ' + cookId,
+									function (error, resultss, fields) {
+										connection.release();
+										if (error) throw error;
+										let response = [
+											{ ...results[0] },
+											{ ...resultss[0] },
+										];
+										res.status(201).json({
+											status: 201,
+											result: response,
+										});
+									}
+								);
+							}
+						);
+					}
+				}
+			);
+		});
+	},
 	// UC-302
 	getAllMeals: (req, res, next) => {
 		dbconnection.getConnection(function (error, connection) {
@@ -55,11 +178,118 @@ let controller = {
 		});
 	},
 	// UC-304
-	updateMealById: (req, res, next) => {},
+	updateMealById: (req, res, next) => {
+		const meal = req.body;
+		const mealId = req.params.mealId;
+		const userId = req.userId;
+
+		logger.debug(`meal with ID ${mealId} requested to be updated`);
+
+		dbconnection.getConnection(function (error, connection) {
+			if (error) throw error;
+			connection.query(
+				'UPDATE meal SET name = ?, description = ?, isActive = ?, isVega = ?, isVegan = ?, isToTakeHome = ?, dateTime = ?, maxAmountOfParticipants = ?, price = ?, imageUrl = ? WHERE id =?;',
+				[
+					meal.name,
+					meal.description,
+					meal.isActive,
+					meal.isVega,
+					meal.isVegan,
+					meal.isToTakeHome,
+					meal.dateTime,
+					meal.maxAmountOfParticipants,
+					meal.price,
+					meal.imageUrl,
+					mealId,
+				],
+				function (error, result, fields) {
+					if (result.affectedRows < 1) {
+						res.status(401).json({
+							status: 401,
+							message: `Meal with ID ${mealId} not found`,
+						});
+						next(error);
+						return;
+					}
+					if (result.affectedRows > 0) {
+						logger.debug(
+							`meal with ID ${mealId} succesfully updated`
+						);
+						// Get updated meal
+						connection.query(
+							'SELECT * FROM meal WHERE id = ' + mealId,
+							function (error, meal, fields) {
+								if (error) throw error;
+
+								// Get user from updated meal
+								connection.query(
+									'SELECT * FROM user WHERE id = ?',
+									[userId],
+									function (error, user, fields) {
+										connection.release();
+										if (error) throw error;
+
+										let response = [
+											{ ...meal[0] },
+											{ ...user[0] },
+										];
+
+										res.status(201).json({
+											status: 201,
+											result: response,
+										});
+									}
+								);
+							}
+						);
+					}
+				}
+			);
+		});
+	},
 	// UC-305
-	deleteMealById: (req, res, next) => {},
-	// UC-306
-	paricipateMeal: (req, res, next) => {},
+	deleteMealById: (req, res, next) => {
+		const mealId = req.params.mealId;
+		const userId = req.userId;
+
+		dbconnection.getConnection(function (error, connection) {
+			// Get Meal before deleting
+			connection.query(
+				'SELECT * FROM meal WHERE id = ' + mealId,
+				function (error, meal, fields) {
+					if (error) throw error;
+					if (meal.length < 1) {
+						res.status(401).json({
+							status: 401,
+							message: 'Meal not found!',
+						});
+						logger.debug('Deleting meal was not found!');
+						return;
+					}
+					// Get owner of meal before deleting
+					connection.query(
+						'SELECT * FROM user WHERE id = ' + userId,
+						function (error, cook, fields) {
+							if (error) throw error;
+							// Delete meal
+							connection.query(
+								'DELETE IGNORE FROM meal WHERE id = ' + mealId,
+								function (error, result, fields) {
+									logger.debug('Meal deleted succesfully!');
+									let response = [...meal, ...cook];
+									connection.release;
+									res.status(201).json({
+										status: 201,
+										result: response,
+									});
+								}
+							);
+						}
+					);
+				}
+			);
+		});
+	},
 };
 
 module.exports = controller;
--- src/routes/meal.routes.js
+++ src/routes/meal.routes.js
@@ -10,14 +10,35 @@ router.get('/', (req, res) => {
 	});
 });
 
-router.post('/api/meal', mealController.registerMeal);
+// UC-301
+router.post(
+	'/api/meal',
+	authController.validateToken,
+	mealController.validateMeal,
+	mealController.registerMeal
+);
 
+// UC-302
 router.get('/api/meal', mealController.getAllMeals);
 
+// UC-303
 router.get('/api/meal/:mealId', mealController.getMealById);
 
-router.put('/api/meal/:mealId', mealController.updateMealById);
+// UC-304
+router.put(
+	'/api/meal/:mealId',
+	authController.validateToken,
+	authController.validateOwnership,
+	mealController.validateMealUpdate,
+	mealController.updateMealById
+);
 
-router.delete('/api/meal/:mealId', mealController.deleteMealById);
+// UC-305
+router.delete(
+	'/api/meal/:mealId',
+	authController.validateToken,
+	authController.validateOwnership,
+	mealController.deleteMealById
+);
 
 module.exports = router;
